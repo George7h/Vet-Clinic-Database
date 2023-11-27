59f66720c784c57fd7d4b81282f502477f540f0c
Austin Obimbo <obimboausts@gmail.com>
2023-11-27 17:13:42 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -53,3 +53,5 @@ ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 CREATE INDEX index_animal_id ON visits(animal_id asc);
 
 CREATE INDEX index_email ON owners(email asc);
+
+CREATE INDEX index_vet_id ON visits(vet_id asc);
